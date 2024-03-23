8e7524462f5166f95f6bb9a5c6489a61a99f6906
joaopma <joaopaulo.m@terra.com.br>
2024-03-22 21:14:37 -0300

    add summary

--- partial-class/Program.cs
+++ partial-class/Program.cs
@@ -5,6 +5,11 @@ public partial class Coords
     private int x;
     private int y;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Coords"/> class with the specified coordinates.
+    /// </summary>
+    /// <param name="x">The x-coordinate.</param>
+    /// <param name="y">The y-coordinate.</param>
     public Coords(int x, int y)
     {
         this.x = x;
